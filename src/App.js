8bcc489275db87ad962e667b4edd7329b4902afb
Vaishnawi Dighore <92801895+VaishnawiDighore@users.noreply.github.com>
2023-01-12 13:55:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Header from "./components/Header";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Detail from "./components/Detail";
+import Footer from "./components/Footer";
+import Search from "./components/Search";
+import Home from "./components/Home";
+
+function App() {
+  
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/image/:id" element={<Detail />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
